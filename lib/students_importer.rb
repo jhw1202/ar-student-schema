3bd7084b76def355872652ba011b5d6fcbe5436f
Apprentice <apprentice@dbc25.local>
2013-03-07 16:53:25 -0800

    adds students and fake teachers to tables

--- lib/students_importer.rb
+++ lib/students_importer.rb
@@ -1,6 +1,7 @@
 require_relative '../app/models/student'
+require_relative '../app/models/teachers'
 
-module StudentsImporter
+module StudentsTeachersImporter
   def self.import(filename=File.dirname(__FILE__) + "/../db/data/students.csv")
     field_names = nil
     Student.transaction do
@@ -14,5 +15,16 @@ module StudentsImporter
         end
       end
     end
+
+    9.times do 
+    Teacher.create(
+      :first_name => Faker::Name.first_name,
+      :last_name => Faker::Name.last_name,
+      :birthday => Date.new(1989,9,24),
+      :gender => ['male','female'].sample,
+      :email => Faker::Internet.email,
+      :phone => Faker::PhoneNumber.phone_number)
   end
+end
+
 end
